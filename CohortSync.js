ccb8a34d77ce8aca7266eb1b59da17e9ce7c28c6
tboulund <tbmh@easv.dk>
2025-01-22 12:23:40 +0100

    Cohorts synced for F2025

--- CohortSync.js
+++ CohortSync.js
@@ -5,31 +5,28 @@ fixture `Moodle Cohort Sync`
     .page `https://moodle.easv.dk/login/index.php`;
 
 var courses = [
-    // { id: 3983, cohorts: [ "DMU-E-ESB24" ] },
-    // { id: 3995, cohorts: [ "DMU-E-ESB24" ] },
-    // { id: 4306, cohorts: [ "DMU-E-ESB24-I" ] },
-    // { id: 3994, cohorts: [ "DMU-E-ESB24-I" ] },
-    // { id: 3982, cohorts: [ "DMU-E-ESB23", "DMU-E-ESB23-I" ] },
-    // { id: 4307, cohorts: [ "DMU-E-ESB23", "DMU-E-ESB23-I" ] },
-    // { id: 4237, cohorts: [ "PBS-F-ESB24", "PBS-F-ESB24-I", "PBS-E-ESB24", "PBS-E-ESB24-I" ] },
-    // { id: 4238, cohorts: [ "PBS-F-ESB24", "PBS-F-ESB24-I", "PBS-E-ESB24", "PBS-E-ESB24-I" ] },
-    // { id: 4239, cohorts: [ "PBS-F-ESB24", "PBS-F-ESB24-I", "PBS-E-ESB24", "PBS-E-ESB24-I" ] },
-    // { id: 3981, cohorts: [ "PBS-F-ONL24", "PBS-E-ONL24" ] },
-    // { id: 4241, cohorts: [ "PBS-F-ONL24", "PBS-E-ONL24" ] },
-    // { id: 4242, cohorts: [ "PBS-F-ONL24", "PBS-E-ONL24" ] },
-    // { id: 3988, cohorts: [ "DMU-E-TØN24" ] },
-    // { id: 3989, cohorts: [ "DMU-E-TØN24" ] },
-    // { id: 3993, cohorts: [ "DMU-E-TØN24" ] },
-    // { id: 3990, cohorts: [ "DMU-E-TØN23" ] },
-    // { id: 3991, cohorts: [ "DMU-E-TØN23" ] },
-    // { id: 3992, cohorts: [ "DMU-E-TØN23" ] },
-
-    // { id: 4240, cohorts: [ "PBS-E-ESB23", "PBS-E-ONL23" ] },
-    // { id: 4084, cohorts: [ "DMU-E-ESB24" ] },
-    // { id: 4085, cohorts: [ "DMU-E-ESB24-I" ] },
-    // { id: 4087, cohorts: [ "DMU-E-TØN24" ] },
-    // { id: 4106, cohorts: [ "PBS-E-ESB23", "PBS-E-ESB23-I" ] },
-    // { id: 4107, cohorts: [ "PBS-E-ONL24" ] },
+    { id: 5014, cohorts: ['DevOps udviklingsmetode (DMU-E-ESB23)', 'DevOps Udviklingsmetode (DMU-E-ESB23-I)', 'DevOps udviklingsmetode (DMU-E-TØN23)'] }, 
+    { id: 5015, cohorts: ['PBS-E-ESB24', 'PBS-E-ESB24-I', 'PBS-F-ESB25', 'PBS-F-ESB25-I'] }, 
+    { id: 5016, cohorts: ['DMU-E-ESB24'] }, 
+    { id: 5017, cohorts: ['DMU-E-ESB24-I'] }, 
+    { id: 5018, cohorts: ['DMU-E-ESB24'] }, 
+    { id: 5019, cohorts: ['DMU-E-ESB24-I'] }, 
+    { id: 5020, cohorts: ['Applications of artificial intelligence (DMU-E-ESB23)', 'Applications of artificial intelligence (DMU-E-ESB23-I)'] }, 
+    { id: 5021, cohorts: ['DMU-E-ESB24'] }, 
+    { id: 5022, cohorts: ['DMU-E-ESB24-I'] }, 
+    { id: 5023, cohorts: ['DMU-E-TØN24'] }, 
+    { id: 5024, cohorts: ['DMU-E-TØN23'] }, 
+    { id: 5026, cohorts: ['PBS-F-ONL25', 'PBS-E-ONL24'] }, 
+    { id: 5027, cohorts: ['Mobilprogrammering (DMU-E-ESB23)', 'Mobilprogrammering (DMU-E-ESB23-I)'] }, 
+    { id: 5028, cohorts: ['PBS-F-ONL25', 'PBS-E-ONL24'] }, 
+    { id: 5029, cohorts: ['PBS-F-ONL25', 'PBS-E-ONL24'] }, 
+    { id: 5030, cohorts: ['Full stack udvikling (DMU-E-ESB23)', 'Full Stack udvikling (DMU-E-ESB23-I)'] }, 
+    { id: 5031, cohorts: ['Tingenes internet (DMU-E-ESB23)', 'Tingenes internet (DMU-E-ESB23-I)'] }, 
+    { id: 5032, cohorts: ['PBS-E-ESB24', 'PBS-E-ESB24-I', 'PBS-F-ESB25', 'PBS-F-ESB25-I'] }, 
+    { id: 5033, cohorts: ['PBS-E-ESB24', 'PBS-E-ESB24-I', 'PBS-F-ESB25', 'PBS-F-ESB25-I'] }, 
+    { id: 5082, cohorts: ['Cybersikkerhed (DMU-E-ESB23)', 'Cybersikkerhed (DMU-E-ESB23-I)', 'Cybersikkerhed (DMU-E-TØN23)'] }, 
+    { id: 5083, cohorts: ['DMU-E-TØN24'] }, 
+    { id: 5084, cohorts: ['DMU-E-TØN24'] }
 ];
 
 test('Moodle Cohort Sync', async t => {
@@ -49,8 +46,7 @@ test('Moodle Cohort Sync', async t => {
 
             await t
                 .typeText(Selector("[id^='form_autocomplete_input-']"), cohort, { replace: true })
-                .wait(1000)
-                .pressKey('enter');
+                .click(Selector("ul.form-autocomplete-suggestions li").withExactText(cohort))
         }
 
         await t
